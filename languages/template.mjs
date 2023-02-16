a0685b7655f68868fd9ce4d7a74050af7f3dadd4
KorniKokosnus <125543982+KorniKokosnus@users.noreply.github.com>
2023-02-16 11:06:23 +0100

    bosanski
    I hope you will now have much more users. 
    
    Signed-off-by: KorniKokosnus <125543982+KorniKokosnus@users.noreply.github.com>

--- languages/template.mjs
+++ languages/template.mjs
@@ -8,65 +8,65 @@
 export default {
     // Pages
     pages: {
-        schedule: 'Schedule',
-        settings: 'Settings',
-        help: 'Help',
-        storedData: 'Stored Data',
-        privacyPolicy: 'Privacy Policy'
+        schedule: 'Raspored',
+        settings: 'Postavke',
+        help: 'Pomoć',
+        storedData: 'Sačuvani podaci',
+        privacyPolicy: 'Pravila privatnosti'
     },
 
     // Schedule page
     //
-    plans_header: 'Select a reading plan',
+    plans_header: 'Izaberi plan čitanja',
     // Reading plans
     plans: {
         // In the order the chapters appear in the Bible
-        sequential: 'Sequential',
+        sequential: 'Po redoslijedu u Bibliji',
         // In the order the Bible books were written
-        written: 'As written',
+        written: 'Po vremenu pisanja',
         // In the order the events happened
-        chronological: 'Chronological'
+        chronological: 'Kronološki'
     },
-    durations_header: 'Duration',
+    durations_header: 'Trajanje',
     setRead: {
-        label: 'Set Read',
-        question: 'Do you want to set all previous sections to read and those following to unread?'
+        label: 'Postavi kao pročitano',
+        question: 'Želite li sve prethodne odlomke postaviti kao pročitane a sljedeće nepročitane?'
     },
 
     // Settings page
     //
-    english: 'English',
-    display: 'Display',
+    english: 'engleski',
+    display: 'Prikaz',
     language: {
-        label: 'Language',
-        placeholder: 'Language',
-        title: 'Choose a language',
-        contribute: 'Contribute Translation'
+        label: 'jezik',
+        placeholder: 'jezik',
+        title: 'Izaberi jezik',
+        contribute: 'Doprinesi prevodu'
     },
     startOver: {
-        label: 'Start Over',
-        title: 'Start over',
-        description: 'Clear the reading status.',
-        question: 'Do you want to clear the reading status?'
+        label: 'Počni ponovo',
+        title: 'Počni ponovo',
+        description: 'Izbriši status čitanja.',
+        question: 'Želite li izbrisati status čitanja?'
     },
     resetEndDate: {
-        label: 'Reset End Date',
-        description: 'Reset the end date to be on time with reading.',
-        question: 'Reset the end date to be on time with reading?',
+        label: 'Ponovo postavite krajnji datum',
+        description: 'Ponovo postavite krajnji datum kako bi vaše čitanje bilo na vrijeme.',
+        question: 'Želite li ponovo postaviti krajnji datum kako bi vaše čitanje bilo na vrijeme?',
     },
     withEndDate: {
-        label: 'With End Date',
-        description: 'Track daily reading with an end date.',
+        label: 'S krajnjim datumom',
+        description: 'Prati dnevno čitanje s krajnjim datumom.',
     },
     showEvents: {
-        label: 'Show Events (experimental)',
+        label: 'Prikaži događaje (eksperimentalno)',
         // Below information can be found in the WOL at the provided link
         // after switching to your language
         URL: 'https://wol.jw.org/en/wol/d/r1/lp-e/1101990130#h=252',
-        text: 'Events are based on',
-        title: '“All Scripture Is Inspired of God and Beneficial”',
-        chapter: 'Study Number 3—Measuring Events in the Stream of Time',
-        caption: 'Chart of outstanding historical dates',
+        text: 'Događaji se temelje na',
+        title: '“Svo je pismo nadahnuto od Boga i korisno”',
+        chapter: '3. dodataka — Vremenski raspored događaja',
+        caption: 'Kronološki raspored važnih događaja iz Biblije',
     },
     // Symbols: a. for “after”; b. for “before”; c. for “circa”, or “about”.
     //          C.E. for Common Era; B.C.E. for Before Common Era
@@ -74,56 +74,55 @@ export default {
         'a.': 'a.',
         'b.': 'b.',
         'c.': 'c.',
-        'C.E.': 'C.E.',
-        'B.C.E.': 'B.C.E.'
+        'n.e.': 'naše ere',
+        'p.n.e.': 'prije naše ere'
     },
     showLocations: {
-        label: 'Show Locations (experimental)',
+        label: 'Prikaži lokacije (eksperimentalno)',
         URL: 'https://wol.jw.org/en/wol/d/r1/lp-e/1102003130',
-        text: 'Show location references to the brochure',
-        title: '“See the Good Land”',
-        legend: 'For a legend see the',
-        caption: 'Index to Maps',
+        text: 'Prikaži lokaciju referenci za brošuru',
+        title: '“Dobra zemlja”',
+        legend: 'Za legendu pogledaj',
+        caption: 'Indeks karti',
     },
-    legal: 'Legal',
-    version: 'Version',
-    help: 'Help',
+    legal: 'Pravno',
+    version: 'Verzija',
+    help: 'Pomoć',
 
 
     // Help page
     //
     helpPage: [
-        'About',
-        'NWT Reading helps you to keep track of your reading schedule of the \
-            New World Translation of the Holy Scriptures of Jehovah\'s Witnesses.',
+        'O aplikaciji',
+        'NWT Reading vam pomaže pratiti svoj raspored čitanja prevoda Biblije Novi Svijet koji su izdali Jehovini svjedoci.',
 
-        'How it Works',
-        'Choose a reading plan and the duration at the top of the page.',
-        'Check the section that you have read.',
-        'After finishing reading the Bible select "Start Over" on the Settings page.',
+        'Kako to funkcionira',
+        'Izaberite plan čitanja i trajanje na vrhu stranice.',
+        'Označite odlomak koji ste pročitali.',
+        'Nakon što pročitate cijelu Bibliju izaberite "Počni ponovo" na stranici "Postavke".',
     ],
 
     // Stored Data page
     //
     storedDataPage: [
-        'This is a dump of all the data that is stored in the app. \
-            It can be useful for troubleshooting and privacy checking.'
+        'Ovo je odlagalište svih podataka pohranjenih u aplikaciji. \
+            To vam može koristiti u slučaju nekih problema ili provjere privatnosti.'
     ],
 
     // Privacy Policy page
     //
     privacyPolicyPage: [
-        'The NWT Reading mobile application (the “PRODUCT”) does not collect \
-        any personal information. All data is stored locally.',
+        'Mobilna aplikacija NWT Reading (u daljem tekstu “PROIZVOD”) ne sakuplja \
+        bilo koje lične informacije. Svi podaci se pohranjuju lokalno.',
 
-        'Searchwork.org, does not allow advertising companies to collect data through \
-        our PRODUCT for ad targeting. \
-        We do not share data collected by this PRODUCT with other organizations \
-        for any purpose.',
+        'Searchwork.org, ne dozvoljava oglašivačima sakupljanje podataka kroz \
+        naš PROIZVOD u svrhu ciljanja oglasa. \
+        Podatke koje je sačuvao ovaj PROIZVOD ne dijelimo s drugim organizacijama  \
+        u bilo koje svrhe.',
 
-        'This privacy policy was last updated on 05/04/2019. \
-        Our privacy policy may change from time to time. \
-        You are advised to review this Privacy Policy periodically for any changes. \
-        Changes to this Privacy Policy are effective when they are posted on this page.'
+        'Ova pravila o privatnosti su ažurirana 05/04/2019. \
+        Naša se Pravila o privatnosti mogu promijeniti s vremena na vrijeme. \
+        Savjetujemo vam da s vremena na vrijeme pogledate ima li promjena u pravilima. \
+        Promjene u ovim pravilima o privatnosti stupaju na snagu onog momenta kad se objave na ovoj stranici.'
     ]
 };
